f8d7983b49fa35508cf80481036fcd563eb7ad9a
plasma-effect <dtyazsk_windows7@yahoo.co.jp>
2014-08-08 22:11:32 +0900

    fix static_assert.hpp

--- test.cpp
+++ test.cpp
@@ -1,15 +1,19 @@
 #pragma message(__TIME__)
 #pragma message(__DATE__)
 
+#include<utility>
 #include<plasma/static_assert.hpp>
-#include<iostream>
 
-constexpr int fact(int N)
+template<std::size_t N>struct fact
 {
-    return (N == 0 ? 1 : N * fact(N - 1));
-}
+	static const int value = N * fact<N - 1>::value;
+};
+template<>struct fact < 0 >
+{
+	static const int value = 1;
+};
 
 int main()
 {
-    PLASMA_PP_STATIC_ASSERT_1NUM(false,"fact(5) == ",fact(5),);
+	PLASMA_PP_STATIC_ASSERT_1NUM(false, "fact<5>::value == ", fact<5>::value, );
 }
